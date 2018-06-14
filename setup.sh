f79c401e22a5c1f816cbaff8bd5a5bd4f57a98d1
Nataila <ccfive814@gmail.com>
2018-06-14 18:40:06 +0800

    ln vim

--- setup.sh
+++ setup.sh
@@ -3,14 +3,36 @@
 
 OH_MY_ZSH=$HOME"/.oh-my-zsh"
 
-if [ ! -d "/bin/zsh" ]; then
+which zsh > /dev/null
+if [ $? -eq 1 ]; then
     sudo apt-get install zsh
     chsh -s /bin/zsh
-fi
-
-if [ ! -d $OH_MY_ZSH ]; then
 fi
 
 install_oh_my_zsh() {
     sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
 }
+
+create_softlink() {
+    from_file=$1
+    to_file=$2
+    echo $from_file, $to_file
+    if [ ! -f "$to_file" ];then
+        ln -s $PWD/$from_file $to_file
+        echo "Makee soft link from $from_file to $to_file"
+    else
+        read -p "$to_file is exists, continue will remove your file, continue? [y or n]" _remove
+        if [[ $_remove = "y" ]]; then
+            rm -fr $to_file
+            ln -s $PWD/$from_file $to_file
+            echo "Make soft link from $from_file to $to_file"
+        fi
+    fi
+}
+
+if [ ! -d $OH_MY_ZSH ];then
+    install_oh_my_zsh
+fi
+
+create_softlink "vim/vimrc" $HOME/".vimrc"
+
