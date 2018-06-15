a4e715795ade8b302ee29ed7a2a0781465a95046
Nataila <ccfive814@gmail.com>
2018-06-15 17:11:38 +0800

    Vundle, pathogen

--- setup.sh
+++ setup.sh
@@ -2,37 +2,59 @@
 # cc @ 2018-06-14 14:55:54
 
 OH_MY_ZSH=$HOME"/.oh-my-zsh"
+VUNDLE=$HOME"/.vim/bundle/Vundle.vim"
+PATHOGEN=$HOME"/.vim/autoload/pathogen.vim"
 
 which zsh > /dev/null
 if [ $? -eq 1 ]; then
-    sudo apt-get install zsh
-    chsh -s /bin/zsh
+  sudo apt-get install zsh
+  chsh -s /bin/zsh
 fi
 
 install_oh_my_zsh() {
-    sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
+  sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
 }
 
 create_link() {
-    from_file=$1
-    to_file=$2
-    echo $to_file
-    if [ ! -f "$to_file" ];then
-        ln -s $PWD/$from_file $to_file
-        echo "Make soft link from $from_file to $to_file"
-    else
-        read -p "$to_file is exists, continue will remove your file, continue? [y or n]" _remove
-        if [[ $_remove = "y" ]]; then
-            rm -fr $to_file
-            ln -s $PWD/$from_file $to_file
-            echo "Make soft link from $from_file to $to_file"
-        fi
+  from_file=$1
+  to_file=$2
+  echo $to_file
+  if [ ! -f "$to_file" ];then
+    ln -s $PWD/$from_file $to_file
+    echo "Make soft link from $from_file to $to_file"
+  else
+    read -p "$to_file is exists, continue will remove your file, continue? [y or n]" _remove
+    if [[ $_remove = "y" ]]; then
+      rm -fr $to_file
+      ln -s $PWD/$from_file $to_file
+      echo "Make soft link from $from_file to $to_file"
     fi
+  fi
 }
 
 if [ ! -d $OH_MY_ZSH ];then
-    install_oh_my_zsh
+  install_oh_my_zsh
 fi
+
+install_pathogen() {
+  if [ ! -d "${VUNDLE}" ]; then
+    mkdir -p ~/.vim/autoload ~/.vim/bundle && \
+    curl -LSso ~/.vim/autoload/pathogen.vim https://tpo.pe/pathogen.vim
+  fi
+}
+
+install_vundle(){
+  if [ -d "${VUNDLE}" ]; then
+    vim +PluginInstall +qall
+  else
+    echo "${VUNDLE} not exists. Git clone to create..."
+    git clone https://github.com/gmarik/Vundle.vim.git ${VUNDLE}
+    vim +PluginInstall +qall
+  fi
+}
+
+install_pathogen
+install_vundle
 
 create_link "vim/vimrc" $HOME/".vimrc"
 create_link "tmux/tmux.conf" $HOME/".tmux.conf"
